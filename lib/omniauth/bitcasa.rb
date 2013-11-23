483e1c8d9cc5f647e40f12a7984074cec174acea
Guilherme Vinicius Moreira <gui.vinicius@gmail.com>
2013-11-23 10:52:14 -0200

    Initial commit

---
+++ lib/omniauth/bitcasa.rb
@@ -0,0 +1,7 @@
+require "omniauth/bitcasa/version"
+
+module Omniauth
+  module Bitcasa
+    # Your code goes here...
+  end
+end
---
+++ lib/omniauth/bitcasa/version.rb
@@ -0,0 +1,5 @@
+module Omniauth
+  module Bitcasa
+    VERSION = "0.0.1"
+  end
+end
